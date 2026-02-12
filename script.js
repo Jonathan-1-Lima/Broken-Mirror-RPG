4e7112809663949d92c2665eb6be8807c12aa980
Jonathan-1-Lima <jk.aquitem@gmail.com>
2026-02-11 20:35:03 -0400

    atualizei o erro do  }, que faltava o avatar vodoll

--- script.js
+++ script.js
@@ -211,7 +211,7 @@ const characters = {
     3: "img/avatar/vodoll/vodoll-emote3-96x96.png",
     4: "img/avatar/vodoll/vodoll-emote4-96x96.png",
     sit: "img/avatar/vodoll/vodoll-sentado-96x96.png"
-  }
+  },
 },
 
 
